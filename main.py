3b06de6515bb5e19bc15a383a901f305e197da23
Nicolas Ezequiel Lugones <lugones.nicolas@gmail.com>
2022-05-27 10:39:42 -0300

    Repo final

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ links_to_transform = {
     'museos' : 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/archivo/4207def0-2ff7-41d5-9095-d42ae8207a5d',
 }
 
-"""codificamos la fecha para poder usarlas en la creacion de carpetas y archivos"""
+"""Codificamos la fecha para poder usarlas en la creacion de carpetas y archivos"""
 today = datetime.date.today().strftime('-%d-%m-%Y')
 year_month = datetime.date.today().strftime('%Y-%m') 
 
-"""Funcion que toma el diccionario, toma los 2 items y los transforma en variables.
-luego llama a la url con requests y la acomoda con BeautifulSoup, filtramos las etiquetas hasta encontrar nuestros links de descarga.
-Hacemos uso de reques para descargar el link del archivo csv, definimos la ruta codificada con nombre y fecha, creamos el directorio, y los archivos correspondientes.
+"""Funcion que toma el diccionario con los links, toma los 2 items.
+Luego llama a la url con requests y la acomoda con BeautifulSoup, filtramos las etiquetas hasta encontrar nuestros links de descarga.
+Hacemos uso de requests para descargar el link del archivo csv, definimos la ruta codificada con nombre y fecha, creamos el directorio, y los archivos correspondientes.
 """
 try:
     dirs_dict = {}
@@ -52,44 +52,47 @@ try:
     logging.debug(f'Se descargaron los archivos y se crearon las carpetas correctamente.')
 
 except Exception as e:
-    logging.exception(f'Hubo una excepcion: {e}')
+    logging.exception(f'No se encontraron los links: {e}')
 
 
+"""Limpiamos nuestros Data Frames, renombramos y exportamos"""
 for i,k in dirs_dict.items(): 
-        if i == 'bibliotecas':
-            df = pd.read_csv(k,sep=',', header=0,encoding='UTF-8')
-            df = df.rename(configdf.bibliotecasRename,axis=1)
-            df.astype({'web': 'object'}).dtypes
-            df['año_inicio'] = df['año_inicio'].fillna(0)
-            df['año_inicio'] = df['año_inicio'].round(decimals=0).astype(int)
-            df.loc[df.año_inicio==0,'año_inicio']='s/d'
-            df = df.fillna('s/d')
-            df.drop_duplicates()
-            df.to_csv(k, index=None)
+    if i == 'bibliotecas':
+        df = pd.read_csv(k,sep=',', header=0,encoding='UTF-8')
+        df = df.rename(configdf.bibliotecasRename,axis=1)
+        df.astype({'web': 'object'}).dtypes
+        df['año_inicio'] = df['año_inicio'].fillna(0)
+        df['año_inicio'] = df['año_inicio'].round(decimals=0).astype(int)
+        df.loc[df.año_inicio==0,'año_inicio']='s/d'
+        df = df.fillna('s/d')
+        df.drop_duplicates()
+        df.to_csv(k, index=None)
 
-        elif i == 'cines':
-            df = pd.read_csv(k,sep=',', header=0,encoding='UTF-8')
-            df = df.rename(configdf.cinesRename,axis=1)
-            df['espacio_INCAA'] = df['espacio_INCAA'].fillna('no')
-            df.loc[df.espacio_INCAA==0, 'espacio_INCAA'] = 'no'
-            df = df.fillna('s/d')
-            df.drop_duplicates()
-            df.replace('0','no')
-            df.to_csv(k, index=None)
+
+    elif i == 'cines':
+        df = pd.read_csv(k,sep=',', header=0,encoding='UTF-8')
+        df = df.rename(configdf.cinesRename,axis=1)
+        df['espacio_INCAA'] = df['espacio_INCAA'].fillna('no')
+        df.loc[df.espacio_INCAA==0, 'espacio_INCAA'] = 'no'
+        df = df.fillna('s/d')
+        df.drop_duplicates()
+        df.replace('0','no')
+        df.to_csv(k, index=None)
     
-        else:
-            i == 'museos'
-            df = pd.read_csv(k,sep=',', header=0,encoding='UTF-8')
-            df = df.rename(configdf.museosRename,axis=1)
-            df['año_inauguracion'] = df['año_inauguracion'].fillna(0)
-            df['año_inauguracion'] = df['año_inauguracion'].round(decimals=0).astype(int)
-            df.loc[df.año_inauguracion==0,'año_inauguracion']='s/d'
-            df = df.fillna('s/d')
-            df.drop_duplicates()
-            df = df.fillna('s/d')
-            df.to_csv(k, index=None)
+    else:
+        i == 'museos'
+        df = pd.read_csv(k,sep=',', header=0,encoding='UTF-8')
+        df = df.rename(configdf.museosRename,axis=1)
+        df['año_inauguracion'] = df['año_inauguracion'].fillna(0)
+        df['año_inauguracion'] = df['año_inauguracion'].round(decimals=0).astype(int)
+        df.loc[df.año_inauguracion==0,'año_inauguracion']='s/d'
+        df = df.fillna('s/d')
+        df.drop_duplicates()
+        df = df.fillna('s/d')
+        df.to_csv(k, index=None)
 
-logging.debug(f'Se renombraron y filtraron los data frames.')
+logging.info(f'Se renombraron los data frames')
+
 
 for i,k in dirs_dict.items(): 
     models.push(i,k)
